848a6c2c6d1b8d0def386e26429b44a807a8e36d
dayday <157325986@qq.com>
2018-04-14 00:09:22 +0800

    头部组件开发完成

---
+++ src/components/Header/index.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import headImg from "../../images/head.jpg";
+import './style.less';
+import "./icon.css"
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <div className="header-contents">
+          <div className="author-infor">
+            <div className="author-head">
+              <img src={headImg} alt="daydayUp"/>
+            </div>
+            <div className="author-title">
+              <div className="aritcle-count">12篇文章</div>
+              <h1>dayday的日志</h1>
+              <ul className="link-list">
+                <li>
+                  <a href="#">
+                    <i className="icon-github"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="icon-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="icon-facebook-square"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="icon-wechat"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="icon-envelope-o"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import Nav from "../../components/Nav"
+import Nav from "../../components/Nav";
+import Header from "../../components/Header";
 import './style.less';
 
 class Home extends Component {
   render() {
     return (
       <div className="App">
-       <Nav />
+        <Nav />
+        <Header />
       </div>
     );
   }
